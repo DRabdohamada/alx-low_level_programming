6980c05f5c8d5f661f7a705e58346627e8f69b9b
DRabdohamada <143115155+DRabdohamada@users.noreply.github.com>
2023-10-27 09:52:17 +0200

    Add files via upload

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of flip need to change ine number to be equal to another number.
+ * @n: first number
+ * @m: second number
+ * Return: the number of bits to make the flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+	unsigned long int xor, rxor;
+	unsigned int n, j;
+
+	n = 0;
+	rxor = 1;
+	xor = n ^ m;
+
+	for (j = 0; j < (sizeof(unsigned long int) * 8); j++)
+	{
+		if (rxor == (xor & rxor))
+			n++;
+
+		rxor <<= 1;
+	}
+
+
+	return (j);
+}
